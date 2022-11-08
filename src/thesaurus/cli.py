ee876713a270a66060dccaf8751d100d798e1d55
Jorge Fernandez <jorgefz.fernandez@gmail.com>
2022-11-08 12:05:30 +0000

    Solved bug with typing annotations, added spelling suggestions

--- src/thesaurus/cli.py
+++ src/thesaurus/cli.py
@@ -3,33 +3,107 @@
 from thesaurus import thesaurus
 import sys
 
+class Color:
+    """ ANSI color codes """
+    BLACK =  "30"
+    RED =    "31"
+    GREEN =  "32"
+    YELLOW = "33"
+    BLUE =   "34"
+    PURPLE = "35"
+    CYAN =   "36"
+    GRAY =   "37"
+    NONE =   "0"
+
+class FontStyle:
+    BOLD = "1"
+    FAINT = "2"
+    ITALIC = "3"
+    UNDERLINE = "4"
+    BLINK = "5"
+    NEGATIVE = "7"
+    CROSSED = "9"
+    NONE = "0"
+
+
+def ansicode(style :str = FontStyle.NONE, color :str = Color.NONE) -> str:
+    """
+    Returns ANSI code with defined font style and color.
+    Example: ansicode(FontStyle.BOLD, Colors.RED) -> "\\033[1;31m"
+    """
+    return f"\033[{style};{color}m"
+
+
+def print_synonyms(defn):
+
+    definition = defn["definition"]
+    synonyms = defn["synonyms"]
+    
+    # Formatting based on level of similarity
+    formats = {}
+    formats["100"] = ansicode(style=FontStyle.BOLD,  color=Color.RED)
+    formats["50"]  = ansicode(style=FontStyle.BOLD,  color=Color.YELLOW)
+    default_fmt = ansicode(style=FontStyle.FAINT, color=Color.GRAY)
+    end_fmt = ansicode()
+
+    print("Definition: ", definition)
+    print("Synonyms:")
+
+    for s in synonyms:
+        term = s['term']
+        sim = s['similarity']
+        color = formats.get(sim, default_fmt)
+        print(" "*10 + color + term + end_fmt)
+    print(" ")
+
 
 def sanitise_input():
 
     if(len(sys.argv) < 2):
-        print("Usage: thesaurus.py [word or expression]")
+        print("Usage:\n\t $ thesaurus [word or expression]")
         exit(-1)
 
     search_term = " ".join(sys.argv[1:])
 
-    all_valid_chars = all(c.isalnum() or c.isspace() for c in search_term)
+    all_valid_chars = all(c.isalnum() or c.isspace() or c == '-' for c in search_term)
 
     if(all_valid_chars is False):
         print("Illegal characters found in search term")
         exit(-1)
     
-    search_term = search_term.replace(" ", r"%20")
-    return search_term
+    search_fmt = search_term.replace(" ", r"%20")
+    return search_term, search_fmt
 
 
 def cli_main():
-    search_term = sanitise_input()
-    print(f"Search term: '{search_term}'")
+    search_term, search_fmt = sanitise_input()
 
-    response = thesaurus.fetch_synonyms(search_term)
+    fmt_relevant = ansicode(FontStyle.UNDERLINE, Color.GREEN)
+    fmt_not_found = ansicode(FontStyle.BOLD, Color.YELLOW)
+    fmt_end = ansicode()
+    max_suggestions = 10
 
-    for defn in response:
-        thesaurus.print_synonyms(defn)
+    print("")
+    print(f"Search term: '{fmt_relevant + search_term + fmt_end}'", end="\n\n")
+    
+    word_list, term_found = thesaurus.fetch_synonyms(search_fmt)
+
+    if term_found is True:
+        for defn in word_list:
+            print_synonyms(defn)
+    
+    else: # Suggest alternate spellings
+        print(f"{fmt_not_found}No results for '{search_term}'{fmt_end}")
+        print("Did you mean...")
+        for i,suggestion in enumerate(word_list):
+            if (i +1 == max_suggestions):
+                break
+            # First suggestion is most relevant
+            if (i==0):
+                print("\t" + fmt_relevant + suggestion['term'] + fmt_end)
+            else:
+                print("\t" + suggestion['term'])
+        print("")
 
 
 
--- src/thesaurus/thesaurus.py
+++ src/thesaurus/thesaurus.py
@@ -2,67 +2,36 @@
 import json
 import requests as reqs
 import sys
+from typing import List
 
 
-class Color:
-    """ ANSI color codes """
-    BLACK =  "30"
-    RED =    "31"
-    GREEN =  "32"
-    YELLOW = "33"
-    BLUE =   "34"
-    PURPLE = "35"
-    CYAN =   "36"
-    GRAY =   "37"
-    NONE =   "0"
-
-class FontStyle:
-    BOLD = "1"
-    FAINT = "2"
-    ITALIC = "3"
-    UNDERLINE = "4"
-    BLINK = "5"
-    NEGATIVE = "7"
-    CROSSED = "9"
-    NONE = "0"
-
-
-def ansicode(style :str = FontStyle.NONE, color :str = Color.NONE) -> str:
+def _fetch_json_from_source(source: str) -> dict:
     """
-    Returns ANSI code with defined font style and color.
-    Example: ansicode(FontStyle.BOLD, Colors.RED) -> "\\033[1;31m"
+    Finds and returns the response from the website in json format
     """
-    return f"\033[{style};{color}m"
+    json_str_start = "<script>window.INITIAL_STATE ="
+    json_str_end   = """\n;</script>\n      </body>\n    </html>\n"""
+
+    json_idx_start = source.find(json_str_start) + len(json_str_start)
+    json_idx_end = len(source) - len(json_str_end) + 1
+
+    if(json_idx_start == -1 or json_idx_end == -1):
+        return None
+
+    json_str = source[json_idx_start:json_idx_end]
+    json_str = json_str.replace("undefined", "null")
+    data = json.loads(json_str)
+
+    return data
 
 
 
-def print_synonyms(defn):
-
-    definition = defn["definition"]
-    synonyms = defn["synonyms"]
-
-    print("Definition: ", definition)
-    print("Synonyms:")
-
-    fmtsyns = []
-    for s in synonyms:
-        term = s['term']
-        sim = s['similarity']
-        if("100" in sim):  color = ansicode(style=FontStyle.BOLD,  color=Color.RED)
-        elif("50" in sim): color = ansicode(style=FontStyle.BOLD,  color=Color.YELLOW)
-        else:              color = ansicode(style=FontStyle.FAINT, color=Color.GRAY)
-        fmtsyns.append( color + term + ansicode() )
-        
-    ncols = 3
-    max_indent = 25
-
-    for i,fs in enumerate(fmtsyns):
-        print("".ljust(10) + fs.ljust(max_indent))
-    print("\n")
-
-
-
-def fetch_synonyms(search_term: str) -> list[dict]:
+def fetch_synonyms(search_term: str) -> List[dict]:
+    """
+    Requests the synonyms for a given search_term
+    and returns a dict object with its definitions
+    and synonyms.
+    """
     
     url_search = f"https://www.thesaurus.com/browse/{search_term}"
     
@@ -72,24 +41,25 @@ def fetch_synonyms(search_term: str) -> list[dict]:
         print("Failed to access thesaurus.com")
         exit(-1)
 
-    if(response.status_code != 200):
-        # Also offer suggestions if not found
-        # "Did you mean..."
-        print("Failed to access thesaurus.com")
+    if( response.status_code == 200):
+        json_response = _fetch_json_from_source(response.text)
+        definitions = json_response["searchData"]["tunaApiData"]["posTabs"]
+        return definitions, True
+
+    elif( response.status_code == 404 ):
+        try:
+            json_response = _fetch_json_from_source(response.text)
+        except:
+            print(f"Failed to access thesaurus.com ({response.status_code})")
+            exit(-1)
+        suggestions = json_response["searchData"]["spellSuggestionsData"]
+        return suggestions, False
+
+    else:
+        print(f"Failed to access thesaurus.com ({response.status_code})")
         exit(-1)
-
-    json_str_search = "<script>window.INITIAL_STATE ="
-    json_str_end = """\n;</script>\n      </body>\n    </html>\n"""
-
-    start = response.text.find(json_str_search) + len(json_str_search)
-    end = len(response.text) - len(json_str_end) + 1
     
-    json_str = response.text[start:end].replace("undefined", "null") 
-    data = json.loads(json_str)
-    
-    definitions = data["searchData"]["tunaApiData"]["posTabs"]
-
-    return definitions
+    return list(), False
 
 
 
